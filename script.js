61d181261a96df8565c88eb91349c0b690be59e8
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-04-17 18:44:42 -0300

    comitando até o desafio 4

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const todasAsCores = document.querySelectorAll("p")
-const vermelho = todasAsCores[0]
-console.log(vermelho)    
+const botao = document.getElementById("button-random-color");
+const cores = document.getElementsByClassName("color");
+const paleta = document.getElementById("color-palette");
+
+botao.addEventListener("click", () => {
+  cores[0].style.backgroundColor = "black";
+  for (let index = 1; index < cores.length; index += 1) {
+    const randomColor = getRandomColor();
+    cores[index].style.backgroundColor = randomColor;
+  }
+});
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
