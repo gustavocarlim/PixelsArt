9a4b8ea883490e765a04896ce5ae9e1d3fc9eff3
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-04-18 11:12:18 -0300

    talvez eu esteja pensando em suicídio

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ const botao = document.getElementById("button-random-color");
 const cores = document.getElementsByClassName("color");
 const paleta = document.getElementById("color-palette");
 
-botao.addEventListener("click", () => {
-  cores[0].style.backgroundColor = "black";
-  for (let index = 1; index < cores.length; index += 1) {
-    const randomColor = getRandomColor();
-    cores[index].style.backgroundColor = randomColor;
-    localStorage.setItem("savepage", JSON.stringify(randomColor));
-  }
-});
-
 function getRandomColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
@@ -18,4 +9,33 @@ function getRandomColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
+function salvandoPaleta() {
+  const paletaCores = [];
+  for (let index = 0; index < cores.length; index += 1) {
+    paletaCores.push(cores[index].style.backgroundColor);
+  }
+  localStorage.setItem("colorPalette", JSON.stringify(paletaCores));
+}
 
+function restaurandoPaleta() {
+  const savedPalette = localStorage.getItem("colorPalette");
+  if (savedPalette) {
+    const savedColors = JSON.parse(savedPalette);
+    for (let i = 0; i < savedColors.length; i++) {
+      cores[i].style.backgroundColor = savedColors[i];
+    }
+  }
+}
+
+botao.addEventListener("click", () => {
+  cores[0].style.backgroundColor = "black";
+  for (let index = 1; index < cores.length; index += 1) {
+    const randomColor = getRandomColor();
+    cores[index].style.backgroundColor = randomColor;
+  }
+  salvandoPaleta();
+});
+
+window.addEventListener("load", () => {
+  restaurandoPaleta();
+});
