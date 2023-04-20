2e3e65c564a3a05aee7055719f78143bee1afe1a
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-04-20 11:57:24 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // Requisito 2
-let cores = document.querySelector("#color-palette");
+let paleta = document.querySelector("#color-palette");
+
+// Requisito 3
+let coresDisponiveis = ["black", "yellow", "pink", "green"];
 
 
-let coresDisponiveis = ["red", "yellow", "pink", "green"];
+const randomButton = document.querySelector('#button-random-color');
+const colorDivs = [];
 
-for (let i = 0; i < coresDisponiveis.length; i++) {
-  let cor = coresDisponiveis[i];
-  { 
-    let corElemento = document.createElement("div");
-    corElemento.className = "color";
-    corElemento.style.backgroundColor = cor;
-    corElemento.style.border = "1px solid black";
-    cores.appendChild(corElemento);
-  }
+for (cores of coresDisponiveis) {
+  const colorDiv = document.createElement('div');
+  colorDiv.className = 'color';
+  colorDiv.style.backgroundColor = cores;
+  paleta.appendChild(colorDiv);
+  colorDivs.push(colorDiv);
 }
+
+
+// Requisito 4
+randomButton.addEventListener('click', () => {
+  const randomColors = [];
+  for (let i = 0; i < 3; i++) {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    randomColors.push(`rgb(${red}, ${green}, ${blue})`);
+  }
+
+  for (let i = 1; i < coresDisponiveis.length; i++) {
+    colorDivs[i].style.backgroundColor = randomColors[i-1];
+  }
+});
