ecc2524a8b61a23d4588ae9fc10cb80473a5ee90
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-04-19 21:08:42 -0300

    refazendo

--- script.js
+++ script.js
@@ -1,74 +1,16 @@
-const botao = document.getElementById("button-random-color");
-const cores = document.getElementsByClassName("color");
-const paleta = document.getElementById("color-palette");
-const bordaDePixel = document.getElementById("pixel-board")
+// Requisito 2
+let cores = document.querySelector("#color-palette");
 
-for (let index = 0; index < 5; index += 1) {
-  const linha = document.createElement('div');
-  linha.className = 'pixel-line';
-  for (let j = 0; j < 5; j++) {
-    const pindexxel = document.createElement('dindexv');
-    pindexxel.className = 'pindexxel';
-    lindexnha.appendChindexld(pindexxel);
-  } 
-  bordaDePindexxel.appendChindexld(lindexnha);
-}
 
-function getRandomColor() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`;
-}
+let coresDisponiveis = ["red", "yellow", "pink", "green"];
 
-function salvandoPaleta() {
-  const paletaCores = [];
-  for (let index = 0; index < cores.length; index += 1) {
-    paletaCores.push(cores[index].style.backgroundColor);
-  }
-  localStorage.setItem("colorPalette", JSON.stringify(paletaCores));
-}
-
-function restaurandoPaleta() {
-  const savedPalette = localStorage.getItem("colorPalette");
-  if (savedPalette) {
-    const savedColors = JSON.parse(savedPalette);
-    for (let index = 0; index < savedColors.length; index += 1) {
-      cores[index].style.backgroundColor = savedColors[index];
-    }
-  }
-  
-  const primeiraCor = document.querySelector('.color');
-  primeiraCor.classList.add('selected');
-
-  for (let i = 0; i < cores.length; i++) {
-    if (cores[i] !== primeiraCor) {
-      cores[i].classList.remove('selected');
-    }
+for (let i = 0; i < coresDisponiveis.length; i++) {
+  let cor = coresDisponiveis[i];
+  { 
+    let corElemento = document.createElement("div");
+    corElemento.className = "color";
+    corElemento.style.backgroundColor = cor;
+    corElemento.style.border = "1px solid black";
+    cores.appendChild(corElemento);
   }
 }
-
-function selecionarCor(event) {
-  const corSelecionada = event.target;
-  
-  if (corSelecionada.classList.contains('color')) {
-    const corAnterior = document.querySelector('.selected');
-    corAnterior.classList.remove('selected');
-    corSelecionada.classList.add('selected');
-  }
-}
-
-paleta.addEventListener('click', selecionarCor);
-
-botao.addEventListener("click", () => {
-  cores[0].style.backgroundColor = "black";
-  for (let index = 1; index < cores.length; index += 1) {
-    const randomColor = getRandomColor();
-    cores[index].style.backgroundColor = randomColor;
-  }
-  salvandoPaleta();
-});
-
-window.addEventListener("load", () => {
-  restaurandoPaleta();
-});
